6a7478c1735fb596cf431ee6159839b82a2302d3
Ł. Sz. <lookszym@gmail.com>
2025-06-17 00:21:37 -0400

    bg

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -19,7 +19,7 @@
         box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
       }
       #hello-container {
-        background: rgba(255, 255, 255, 0.91);
+        background: rgba(255, 255, 255, 1);
         border-radius: 12px;
         box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
         padding: 10px;
