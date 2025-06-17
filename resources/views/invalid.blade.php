8820c28a1d0ca151b8ed12b1956fd007b8f97f26
Ł. Sz. <lookszym@gmail.com>
2025-06-16 23:36:33 -0400

    privacy

--- resources/views/invalid.blade.php
+++ resources/views/invalid.blade.php
@@ -56,7 +56,10 @@
 
     <!--- Footer --->
     <footer id="footer" class="bg-white text-gray-500 text-center py-4 shadow-md transition-all duration-300">
-      © 2025 Adam Mickiewicz University. All rights reserved
+      © 2025 Adam Mickiewicz University. All rights reserved. 
+      <a href="/privacy" target="_blank" rel="noopener noreferrer" class="text-blue-500 hover:underline">
+        Privacy Policy
+      </a>
     </footer>
   
   </body>
